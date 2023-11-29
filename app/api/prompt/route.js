319d3f20050008f1c5b09d68df2410daa0d7de93
egor.kozyrev <egor.kozyrev@newfold.com>
2023-11-29 16:03:30 +0200

    refactor

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,11 +1,10 @@
 import { connectToDB } from "@utils/database";
 import Prompt from "@models/prompt";
 
-export const GET = async (req) => {
+export const GET = async () => {
 	try {
 		await connectToDB();
 		const prompts = await Prompt.find({}).populate('creator')
-		console.log(prompts);
 		return new Response(JSON.stringify(prompts), { status: 200 });
 	} catch (e) {
 		return new Response("Failed to get any prompts", { status: 500 });
