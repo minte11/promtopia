42c72ee298df5fe673161fb75c2ee71de8ecb777
egor.kozyrev <egor.kozyrev@newfold.com>
2023-12-08 13:55:24 +0200

    typo mistake

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
-import Profile from "@components/profile";
+import Profile from "@components/Profile";
 
 const fetchPrompts = async (userId) => {
 	const response = await fetch(`/api/users/${userId}/posts/`);
